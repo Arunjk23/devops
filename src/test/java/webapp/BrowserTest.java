ca8386608fa37cc9c890fd41218295c3066b6881
akumar255 <Arunjk23@gmail.com>
2021-07-21 14:03:47 +0530

    Initial commit

---
+++ src/test/java/webapp/BrowserTest.java
@@ -0,0 +1,31 @@
+package webapp;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+
+public class BrowserTest {
+
+	
+	@Test
+	public void getData()
+	{
+		System.out.println("Hello Guys");
+		System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+		WebDriver driver =new ChromeDriver();
+		driver.get("http://arunkumarj.azurewebsites.net/webapp/");
+		String text =driver.findElement(By.cssSelector("h1")).getText();
+		System.out.println(text);
+		Assert.assertTrue(text.equalsIgnoreCase("DevOps Checking via Azure DevOps"));
+		driver.close();
+	
+		
+		
+	}
+}
